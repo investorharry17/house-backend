0113075abbb3aaaf4e3d1ad469a2b68cef2ff877
AJ <amehharrison2020@gmail.com>
2023-11-24 04:05:58 +0100

    quick commit

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ app.use(compression())
 const corsOptions = {
 	origin : ["http://localhost:5173", 
 		"harry_house_backend.onrender.com",
-		"https://harry-house-backend.onrender.com"
+		"https://harry-house-backend.onrender.com",
+		"https://radiant-kitten-54a8b1.netlify.app"
 	],
 	methods : "*",
 	optionSuccessStauts : 204
